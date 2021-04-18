74fa34a5a941ed4835320b3cf5f1640becb5b758
Zagidziran <38669182+Zagidziran@users.noreply.github.com>
2021-04-18 18:05:17 +0300

    Add files via upload

---
+++ calc.ps1
@@ -0,0 +1,45 @@
+﻿### Простой скрипт выполняющий арифметические операции
+### Комментарии начинаются со знака решетки
+
+# Это бесконечный цикл, но он будет прерван принудительно при вводе "exit"
+while($true)
+{
+    write-host "Для выхода наберите exit"
+
+
+    $firstoperand = read-host "Введите первое число"
+
+    # это ветвление
+    if ($firstoperand -eq "exit")
+    {
+        exit
+    }
+
+    $operation = read-host "Введите операцию ( + или - )"
+
+    # Это ветвление
+    if ($operation -eq "exit")
+    {
+        exit
+    }
+
+
+    $secondoperand = read-host "Введите второе число"
+
+    if ($secondoperand -eq "exit")
+    {
+        exit
+    }
+
+    if ($operation -eq "+")
+    {
+        $result = [int]$firstoperand + [int]$secondoperand
+        write-host Ответ: $result
+    }
+
+    if ($operation -eq "-")
+    {
+        $result = [int]$firstoperand - [int]$secondoperand
+        write-host Ответ: $result    
+    }
+}
